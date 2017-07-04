325e32be9303ba06ed0c7b04b4fc1a1342f1a465
Westermann <westermann93@googlemail.com>
2017-07-04 08:28:37 +0200

    Remove bs file

--- lib/LandscapeModel.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import george as sklearn_gp
-from sklearn import gaussian_process as sklearn_gp
-
-
-class SurrogateModel(object):
-
-    def __init__(self, lib):
-        self.regressor = GaussianProcessRegressor(
